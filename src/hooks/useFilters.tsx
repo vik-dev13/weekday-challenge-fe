e5b6cd8f40dd2dd58c6b097f19a123e0e2c23a33
vikdev-13 <vikdev.13@gmail.com>
2024-05-03 13:43:36 +0530

    added filters

--- src/hooks/useFilters.tsx
+++ src/hooks/useFilters.tsx
@@ -13,15 +13,60 @@ export const useFilters = () => {
 		return arr;
 	};
 
+	const filterByLocation = (jobs: IJob[]): IJob[] => {
+		const { location } = router.query;
+
+		const arr: IJob[] = jobs.filter(
+			(job) => job?.location === String(location),
+		);
+		return arr;
+	};
+
+	const filterByRemoteOrOnsite = (jobs: IJob[]): IJob[] => {
+		const { isRemote } = router.query;
+		const arr: IJob[] = jobs.filter((job) => {
+			if (isRemote === "remote") {
+				return job.location === "remote";
+			}
+			if (isRemote === "onsite") {
+				return job.location !== "remote";
+			}
+		});
+		return arr;
+	};
+
+	const filterByRole = (jobs: IJob[]): IJob[] => {
+		const { role } = router.query;
+		const arr: IJob[] = jobs.filter((job) => job?.jobRole === String(role));
+		return arr;
+	};
+
 	const handleFilter = (jobs: IJob[]) => {
 		const queryArr = Object.keys(router?.query);
+		console.log("query==", queryArr);
 		if (queryArr.length === 0) return [];
-		let jobsArr: IJob[] = [];
+		let jobsArr: IJob[] = [...jobs];
 		// check for minExp
 		if (queryArr.includes("minExp")) {
-			const jdList = filterByMinExp(jobs);
-			jobsArr = [...jobsArr, ...jdList];
+			const filteredJdList = filterByMinExp(jobsArr);
+			jobsArr = [...filteredJdList];
 		}
+
+		if (queryArr.includes("location")) {
+			const filteredJdList = filterByLocation(jobsArr);
+			jobsArr = [...filteredJdList];
+		}
+
+		if (queryArr.includes("isRemote")) {
+			const filteredJdList = filterByRemoteOrOnsite(jobsArr);
+			jobsArr = [...filteredJdList];
+		}
+
+		if (queryArr.includes("role")) {
+			const filteredJdList = filterByRole(jobsArr);
+			jobsArr = [...filteredJdList];
+		}
+
 		return jobsArr;
 	};
 	return { handleFilter };
--- src/hooks/useScroll.tsx
+++ src/hooks/useScroll.tsx
@@ -2,14 +2,6 @@
 
 import * as React from "react";
 
-// console.log("offset----", ref?.current?.offsetHeight);
-// console.log("verticalScroll----", ref?.current?.scrollTop);
-// console.log("innerheight----", ref?.current?.scrollHeight);
-// console.log(
-//     "totalScrollHeight ----",
-//     ref?.current?.offsetHeight + ref?.current?.scrollTop,
-// );
-
 export default function useScroll(
 	ref: React.RefObject<HTMLDivElement>,
 	callback: () => void,
@@ -20,7 +12,6 @@ export default function useScroll(
 				ref?.current?.offsetHeight + ref?.current?.scrollTop >=
 				ref?.current?.scrollHeight
 			) {
-				console.log("fetching--------");
 				callback();
 			}
 		}
